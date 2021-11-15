37b4ee97fb5266233a14ddc26176ebfcc1308acc
Everton Souza <evertonrafaelpessoal@gmail.com>
2021-11-14 23:11:02 -0300

    Find by id, exceptions

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/EntityNotFoundExeception.java
@@ -0,0 +1,14 @@
+package com.devsuperior.dscatalog.services.exceptions;
+
+public class EntityNotFoundExeception extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public EntityNotFoundExeception (String msg) {
+		super(msg);
+	}
+	
+	
+	
+
+}
